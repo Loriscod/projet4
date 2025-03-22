c737ed03ea0aa9c63cbd9ec0a8bfdf264ee9ffc7
Luca Heritiana TOBE <123lucatobe@gmail.com>
2025-03-22 09:36:02 +0300

    .env.example added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion de Pointage</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+
+    
+            
+</body>
+
+</html>
